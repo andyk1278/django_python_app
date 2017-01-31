6668185669e79479751108933f9cbdfe8aac9b1c
andyk1278 <andyk1278@gmail.com>
2017-01-31 01:14:59 -0800

    First commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/droopy/sandbox/treehouse/django-basics/django_python_app/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
