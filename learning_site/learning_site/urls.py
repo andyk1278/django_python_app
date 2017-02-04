850cbca26528eb094cad8087cd668ed9c602c7a3
andyk1278 <andyk1278@gmail.com>
2017-02-04 02:15:51 -0800

    added templates for each view, static assets, migrations and 404s, step detail view, added content to step, and url tags

--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
 urlpatterns = [
-    url(r'^courses/', include('courses.urls')),
+    url(r'^courses/', include('courses.urls', namespace='courses')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home),
+    url(r'^$', views.home, name='home'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
